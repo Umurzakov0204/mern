8ee4c3dce9267d72b64c61ca6ddfc3844412b145
Umurzakov0204 <tomthompson0222@gmail.com>
2022-06-03 23:45:16 +0500

    That's it for today

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,28 @@
 const {Router} = require('express')
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
 const router = Router()
 
+router.post('/register', async (req, res) => {
+    try {
+
+        const {email, password} = req.body
+
+        const candidate = await User.findOne({email})
+
+        if (candidate) {
+            res.status(400).json({message: 'Takoy polzovatel uje est!'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+
+    } catch (e) {
+        return res.status(500).json({ message: "Oshibka, chotta ne tak napisal)" })
+    }
+})
+
+router.post('/login', async (req, res) => {
+
+})
 
 module.exports = router;
