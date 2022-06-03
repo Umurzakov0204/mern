b2a44ade72a696ac6445e6b6cb448193d0358d22
Umurzakov0204 <tomthompson0222@gmail.com>
2022-06-03 17:12:11 +0500

    errors(

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const mongoose = require('mongoose')
 
 const app = express()
 
+app.use('/api/auth', require('./routes/auth.routes'))
 
 const PORT = config.get('port') || 5000
 
-async function start() {
+
+async function  start() {
     try {
-        await mongoose.connect(config.get('mongoUri')), {
+        await mongoose.connect(config.get('mongoUri'), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true
-        }
+            // useCreateIndex: true
+        })
         app.listen(PORT, () => console.log(`App has been started on port ${PORT}`))
     } catch (e) {
         console.log('Server Error', e.message)
